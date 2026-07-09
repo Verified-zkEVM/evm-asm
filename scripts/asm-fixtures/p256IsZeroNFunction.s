0cbdda8ed907668911bee61f5cde78f279872d25
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2026-07-09 17:31:59 +0200

    fix(p256): refresh is-zero-n fixture after merge

--- scripts/asm-fixtures/p256IsZeroNFunction.s
+++ scripts/asm-fixtures/p256IsZeroNFunction.s
@@ -1,13 +1,13 @@
 p256_is_zero_n:
-  mv x6, x10
-  mv x5, x11
-  beq x5, x0, .+24
-  lbu x7, 0(x6)
-  bne x7, x0, .+24
-  addi x6, x6, 1
-  addi x5, x5, -1
+  mv x5, x10
+  mv x6, x11
+  beq x6, x0, .+24
+  lbu x7, 0(x5)
+  bne x7, x0, .+16
+  addi x5, x5, 1
+  addi x6, x6, -1
   jal x0, .-20
   li x10, 1
-  jalr x0, 0(x1)
+  beq x6, x0, .+8
   li x10, 0
   jalr x0, 0(x1)
